d7473bdc3cb2eb8adfb9eff255b85b6204c2c100
santivrca <santivrca@gmail.com>
2024-02-27 20:16:00 -0300

    package modificado

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require("path")
 const publicPath = path.resolve(__dirname, "./public")
 
-app.listen(3000, () =>
-    console.log("Servidor corriendo")
+
+const port = proces.env.PORT || 3001;
+app.listen(port, () =>
+    console.log("Servidor corriendo en el puerto")
 );
 
 app.use(express.static(publicPath));
